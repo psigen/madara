3fa0e2c4951eb52e5e05f633a7f3319af06aa928
jedmondson <jedmondson@2850746a-f1c8-11dd-b507-8b4ecd5210be>
2011-03-04 04:45:23 +0000

    * Fixed issues with Test_Reasoning_Throughput that were causing incorrect reports of rules per second. Throughput has drastically gone up across the board (windows and linux)
    git-svn-id: http://madara.googlecode.com/svn/trunk@212 2850746a-f1c8-11dd-b507-8b4ecd5210be

--- tests/test_reasoning_throughput.cpp
+++ tests/test_reasoning_throughput.cpp
@@ -23,28 +23,28 @@ int parse_args (int argc, ACE_TCHAR * argv[]);
 // test functions
 unsigned long long test_function_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 unsigned long long test_function_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 unsigned long long test_optimal_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 unsigned long long test_optimal_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 unsigned long long test_simple_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 unsigned long long test_simple_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 unsigned long long test_large_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 unsigned long long test_large_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge,
-     unsigned int iterations);
+     unsigned long iterations);
 
 // C++ function for increment with boolean check, rather than allowing C++ 
 // to optimize by putting things in registers
@@ -58,8 +58,8 @@ long increment (long value);
 
 
 // default iterations
-unsigned int num_iterations = 100000;
-unsigned int num_runs = 10;
+unsigned long num_iterations = 100000;
+unsigned long num_runs = 10;
 
 // still trying to stop this darn thing from optimizing the increments
 class Incrementer
@@ -127,7 +127,7 @@ int ACE_TMAIN (int argc, ACE_TCHAR * argv[])
     exit (-1);
   }
 
-  for (unsigned int i = 0; i < num_runs; ++i)
+  for (unsigned long i = 0; i < num_runs; ++i)
   {
     // run tests
     sr_result += test_simple_reinforcement (
@@ -151,14 +151,20 @@ int ACE_TMAIN (int argc, ACE_TCHAR * argv[])
   // to find out the number of iterations per second, we need to 
   // multiply the numerator by the factor of the result, which is
   // given in microseconds.
-  double sr_avg = (double) 1000000 * num_iterations * num_runs / sr_result;
-  double lr_avg = (double) 1000000 * num_iterations * num_runs / lr_result;
-  double si_avg = (double) 1000000 * num_iterations * num_runs / si_result;
-  double li_avg = (double) 1000000 * num_iterations * num_runs / li_result;
-  double oi_avg = (double) 1000000 * num_iterations * num_runs / oi_result;
-  double or_avg = (double) 1000000 * num_iterations * num_runs / or_result;
-  double fi_avg = (double) 1000000 * num_iterations * num_runs / fi_result;
-  double fr_avg = (double) 1000000 * num_iterations * num_runs / fr_result;
+
+  unsigned long evaluations = num_iterations * num_runs;
+
+  // evaluations / sr_result == operations / ns
+  // evaluations / second == operations / ns * 1,000,000,000 ns / s
+
+  double sr_avg = (double) 1000000000 * evaluations / sr_result;
+  double lr_avg = (double) 1000000000 * evaluations / lr_result;
+  double si_avg = (double) 1000000000 * evaluations / si_result;
+  double li_avg = (double) 1000000000 * evaluations / li_result;
+  double oi_avg = (double) 1000000000 * evaluations / oi_result;
+  double or_avg = (double) 1000000000 * evaluations / or_result;
+  double fi_avg = (double) 1000000000 * evaluations / fi_result;
+  double fr_avg = (double) 1000000000 * evaluations / fr_result;
 
   ACE_DEBUG ((LM_INFO, 
     "\nResults for tests with %d iterations were:\n", num_iterations));
@@ -198,47 +204,50 @@ long increment (long value)
 /// Tests logicals operators (&&, ||)
 unsigned long long test_simple_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_simple_reinforcement"));
 
   knowledge.clear ();
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
-  for (unsigned int i = 0; i < iterations; ++i)
+  for (unsigned long i = 0; i < iterations; ++i)
   {
     // test literals in conditionals
     knowledge.evaluate ("++.var1");
   }
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d iter of eval(\"++.var1\") was %d\n", 
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of eval(\"++.var1\") was %d\n", 
     knowledge.get (".var1")));
 
-  return stop - start;
+  return measured;
 }
 
 /// Tests logicals operators (&&, ||)
 unsigned long long test_large_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_large_reinforcement"));
 
   knowledge.clear ();
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
   // build a large chain of simple reinforcements
    std::stringstream buffer;
@@ -246,74 +255,78 @@ unsigned long long test_large_reinforcement (
   unsigned max_size = iterations > 10000 ? 10000 : iterations;
   unsigned actual_iterations = iterations > 10000 ? iterations / 10000 : 1;
   
-  for (unsigned int i = 0; i < max_size; ++i)
+  for (unsigned long i = 0; i < max_size; ++i)
   {
     buffer << "++.var1;";
   }
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
   // execute that chain of reinforcements
-  for (unsigned int i = 0; i < actual_iterations; ++i)
+  for (unsigned long i = 0; i < actual_iterations; ++i)
     knowledge.evaluate (buffer.str ());
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d length reinforcement chain in eval was %d\n",
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of large reinforcement eval was %d\n",
     knowledge.get (".var1")));
 
-  return stop - start;
+  return measured;
 }
 
 /// Tests logicals operators (&&, ||)
 unsigned long long test_simple_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_simple_inference"));
 
   knowledge.clear ();
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
-  for (unsigned int i = 0; i < iterations; ++i)
+  for (unsigned long i = 0; i < iterations; ++i)
   {
     // test literals in conditionals
     knowledge.evaluate ("1 => ++.var1");
   }
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d iter of eval(\"1 => ++.var1\") was %d\n",
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of eval(\"1 => ++.var1\") was %d\n",
     knowledge.get (".var1")));
 
-  return stop - start;
+  return measured;
 }
 
 /// Tests logicals operators (&&, ||)
 unsigned long long test_large_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_large_inference"));
 
   knowledge.clear ();
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
   // build a large chain of simple reinforcements
    std::stringstream buffer;
@@ -321,107 +334,112 @@ unsigned long long test_large_inference (
   unsigned max_size = iterations > 10000 ? 10000 : iterations;
   unsigned actual_iterations = iterations > 10000 ? iterations / 10000 : 1;
   
-  for (unsigned int i = 0; i < max_size; ++i)
+  for (unsigned long i = 0; i < max_size; ++i)
   {
     buffer << "1 => ++.var1;";
   }
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
   // execute that chain of reinforcements
-  for (unsigned int i = 0; i < actual_iterations; ++i)
+  for (unsigned long i = 0; i < actual_iterations; ++i)
     knowledge.evaluate (buffer.str ());
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d length inference chain in eval was %d\n",
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of large inference eval was %d\n",
     knowledge.get (".var1")));
 
-  return stop - start;
+  return measured;
 }
 
 /// Tests logicals operators (&&, ||)
 unsigned long long test_optimal_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_optimal_inference"));
 
   knowledge.clear ();
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
   long var1 = 0;
   bool conditional = true;
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
-  for (unsigned int i = 0; i < iterations; ++i)
+  for (unsigned long i = 0; i < iterations; ++i)
   {
     // don't use the var1 by reference because the compiler appears
     // to optimize it to a register still
     var1 = increment (conditional, var1);
   }
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d optimal C++ inferences was %d\n",
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of optimal C++ inferences was %d\n",
     var1));
 
-  return stop - start;
+  return measured;
 }
 
 /// Tests logicals operators (&&, ||)
 unsigned long long test_optimal_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_optimal_reinforcement"));
 
   knowledge.clear ();
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
   long var1 = 0;
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
-  for (unsigned int i = 0; i < iterations; ++i)
+  for (unsigned long i = 0; i < iterations; ++i)
   {
     // use a function so var1 isn't put into a processor register
     // and super-optimized
     var1 = increment(var1);
   }
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d optimal C++ reinforcements was %d\n",
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of optimal C++ reinforcements was %d\n",
     var1));
 
-  return stop - start;
+  return measured;
 }
 
 /// Tests C++ function inference
 unsigned long long test_function_inference (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_function_inference"));
 
@@ -429,39 +447,41 @@ unsigned long long test_function_inference (
   Incrementer accumulator;
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
   long var1 = 0;
   bool conditional = true;
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
-  for (unsigned int i = 0; i < iterations; ++i)
+  for (unsigned long i = 0; i < iterations; ++i)
   {
     // don't use the var1 by reference because the compiler appears
     // to optimize it to a register still
     accumulator.increment (conditional);
   }
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
 
   var1 = accumulator.value ();
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d functional C++ inferences was %d\n",
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of functional C++ inferences was %d\n",
     var1));
 
-  return stop - start;
+  return measured;
 }
 
 /// Tests C++ function inference
 unsigned long long test_function_reinforcement (
      Madara::Knowledge_Engine::Knowledge_Base & knowledge, 
-     unsigned int iterations)
+     unsigned long iterations)
 {
   ACE_TRACE (ACE_TEXT ("test_function_reinforcement"));
 
@@ -469,32 +489,35 @@ unsigned long long test_function_reinforcement (
   Incrementer accumulator;
 
   // keep track of time
-  ACE_hrtime_t start, stop;
+  ACE_hrtime_t measured;
+  ACE_High_Res_Timer timer;
 
   long var1 = 0;
 
-  start = ACE_OS::gethrtime ();
+  timer.start ();
 
-  for (unsigned int i = 0; i < iterations; ++i)
+  for (unsigned long i = 0; i < iterations; ++i)
   {
     // don't use the var1 by reference because the compiler appears
     // to optimize it to a register still
     accumulator.increment ();
   }
 
-  stop = ACE_OS::gethrtime ();
+  timer.stop ();
+  timer.elapsed_time (measured);
+
 
   var1 = accumulator.value ();
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t) Time for %d functional C++ reinforcements was %d\n",
-    iterations, stop - start));
+    iterations, measured));
 
   ACE_DEBUG ((LM_INFO, 
     "(%P|%t)   Final value of functional C++ reinforcements was %d\n",
     var1));
 
-  return stop - start;
+  return measured;
 }
 
 
