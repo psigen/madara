c3316556e9606bc8ca90247b0784275cbe82dccb
jedmondson <jedmondson@2850746a-f1c8-11dd-b507-8b4ecd5210be>
2011-12-19 01:36:57 +0000

    * Added Doxygen commenting to Thread_Safe_Context.h
    git-svn-id: http://madara.googlecode.com/svn/trunk@441 2850746a-f1c8-11dd-b507-8b4ecd5210be

--- madara/knowledge_engine/Thread_Safe_Context.h
+++ madara/knowledge_engine/Thread_Safe_Context.h
@@ -35,117 +35,230 @@ namespace Madara
     public:
       typedef ACE_Condition <ACE_Recursive_Thread_Mutex> Condition;
 
-      /// Constructor.
+      /**
+       * Constructor.
+       **/
       Thread_Safe_Context ();
 
-      /// Destructor.
+      /**
+       * Destructor.
+       **/
       ~Thread_Safe_Context (void);
 
-      /// Atomically return the value of a variable.
+      /**
+       * Atomically returns the value of a variable.
+       * @param   key    unique identifier of the variable
+       * @return         the long long value for the variable
+       **/
       long long get (const ::std::string & key) const;
 
-      /// Atomically set the value of a variable.
-      /// @return   0 if the value was set. -1 if null key
+      /**
+       * Retrieves a knowledge record from the key. This function is useful
+       * for performance reasons and also for using a Knowledge_Record that
+       * can be one of multiple types
+       * @param   key    unique identifier of the variable
+       * @return         the knowledge record for the variable
+       **/
+      Madara::Knowledge_Record * get_record (const ::std::string & key) const;
+
+      /**
+       * Atomically sets the value of a variable.
+       * @param   key       unique identifier of the variable
+       * @param   value     new value of the variable
+       * @param   modified  send modifications after applying the update
+       * @return   0 if the value was set. -1 if null key
+       **/
       int set (const ::std::string & key, long long value, 
         bool modified = true);
 
-      /// Atomically set if the variable value will be different
-      /// @return   1 if the value was changed. 0 if not changed.
-      ///           -1 if null key
+      /**
+       * Atomically sets if the variable value will be different
+       * @param   key       unique identifier of the variable
+       * @param   value     new value of the variable
+       * @param   modified  send modifications after applying the update
+       * @return   1 if the value was changed. 0 if not changed.
+       *          -1 if null key
+       **/
       int set_if_unequal (const ::std::string & key, long long value, 
         unsigned long quality, unsigned long long clock, 
         bool modified = true);
 
-      /// Get quality of last update to a variable
+      /**
+       * Atomically gets quality of a variable
+       * @param   key       unique identifier of the variable
+       * @return   quality associated with the variable
+       **/
       unsigned long get_quality (const ::std::string & key);
       
-      /// Get quality of local process updates to a variable
+      /**
+       * Atomically gets write quality of this process for a variable
+       * @param   key       unique identifier of the variable
+       * @return   write quality associated with the variable
+       **/
       unsigned long get_write_quality (const ::std::string & key);
       
-      /// Set quality of last update to a variable
+      /**
+       * Atomically sets quality of this process for a variable
+       * @param   key            unique identifier of the variable
+       * @param   quality        quality of this process
+       * @param   force_update   force an update to variable, even if lower
+       * @return   write quality associated with the variable
+       **/
       unsigned long set_quality (const ::std::string & key, 
         unsigned long quality, bool force_update);
-      
-      /// Indicate that a status change has occurred. This could be a message
-      /// from the transport to let the knowledge engine know that new agents
-      /// are available to send knowledge to.
+
+       /**
+       * Force a change to be registered, waking up anyone waiting on entry
+       **/
       void set_changed (void);
 
-      /// Set quality of this process writing to a variable
+      /**
+       * Atomically sets write quality of this process for a variable
+       * @param   key            unique identifier of the variable
+       * @param   quality        write quality of this process
+       **/
       void set_write_quality (const ::std::string & key, unsigned long quality);
 
-      /// Return list of variables that have been modified
+      /**
+       * Retrieves a list of modified variables. Useful for building a
+       * disseminatable knowledge update.
+       * @param   modified       list of modified variables (comma separated)
+       * @param   quality        write quality of this process
+       **/
       void get_modified (Madara::String_Vector & modified) const;
 
-      /// Reset all variables to unmodified
+      /**
+       * Reset all variables to be unmodified. This will clear all global
+       * knowledge updates. Use with caution.
+       **/
       void reset_modified (void);
 
-      /// Changes all global variables to modified at current time
+      /**
+       * Changes all global variables to modified at current clock.
+       **/
       void apply_modified (void);
 
-      /// Reset a variable to unmodified
+      /**
+       * Resets a variable to unmodified
+       * @param   key            unique identifier of the variable
+       **/
       void reset_modified (const ::std::string & key);
 
-      /// Atomically increment the value of the variable
+      /**
+       * Atomically increments the value of the variable
+       * @param   key            unique identifier of the variable
+       * @return                 new value of variable
+       **/
       long long inc (const ::std::string & key);
 
-      /// Make the current thread of execution wait for a change on the
-      /// context
+      /**
+       * Wait for a change to happen to the context.
+       * @param   extra release  perform extra release of lock for nested locks
+       **/
       void wait_for_change (bool extra_release = false);
 
-      /// Atomically decrement the value of the variable
+      /**
+       * Atomically decrements the value of the variable
+       * @param   key            unique identifier of the variable
+       * @return                 new value of variable
+       **/
       long long dec (const ::std::string & key);
 
-      /// Atomically check to see if a key exists
+      /**
+       * Atomically checks to see if a variable already exists
+       * @param   key            unique identifier of the variable
+       * @return                 true if variable exists
+       **/
       bool exists (const ::std::string & key) const;
 
-      /// Print all variables and their values.
+      /**
+       * Atomically prints all variables and values in the context
+       * @param   level          log level. @see Log_Macros.h
+       **/
       void print (unsigned int level) const;
 
-      /// Print a statement, similar to printf (variable expansions allowed)
-      /// e.g. input = "MyVar{.id} = {MyVar{.id}}\n";
+      /**
+       * Atomically Print a statement, similar to printf (variable expansions
+       * allowed) e.g., input = "MyVar{.id} = {MyVar{.id}}\n";
+       * @param   statement      templated statement to print from
+       * @param   level          log level. @see Log_Macros.h
+       **/
       void print (const std::string & statement, unsigned int level) const;
 
-      /// Clear all variables and their values.
+      /**
+       * Clears all variables and their values
+       **/
       void clear (void);
 
-      /// Lock the mutex on this context. Warning: this will cause
-      /// all operations to block until the unlock call is made.
+      /**
+       * Locks the mutex on this context. Warning: this will cause
+       * all operations to block until the unlock call is made.
+       **/
       void lock (void) const;
 
-      /// Unlock the mutex on this context.
+      /**
+       * Unlocks the mutex on this context.
+       **/
       void unlock (void) const;
       
-      /// atomically increment the lamport clock and return the new
-      /// clock time (intended for sending knowledge updates)
+      /**
+       * Atomically increments the Lamport clock and returns the new
+       * clock time (intended for sending knowledge updates).
+       * @return        new clock time
+       **/
       unsigned long long inc_clock (void);
 
-      /// atomically increment the lamport clock of a particular variable
-      /// and return the new clock time (for sending knowledge updates)
+      /**
+       * Atomically increments the Lamport clock of a variable and returns the
+       * new clock time (intended for sending knowledge updates).
+       * @param   key   unique identifier of the variable
+       * @return        new clock time for variable
+       **/
       unsigned long long inc_clock (const std::string & key);
 
-      /// set the lamport clock (updates with lamport clocks lower
-      /// than our current clock get discarded)
+      /**
+       * Atomically sets the lamport clock.
+       * @param  clock  new global clock
+       * @return        new clock time
+       **/
       unsigned long long set_clock (unsigned long long clock);
 
-      /// set the lamport clock for a particular variable (updates with 
-      /// lamport clocks lower than our current clock get discarded)
+      /**
+       * Atomically sets the Lamport clock of a variable and returns the
+       * new clock time (intended for sending knowledge updates).
+       * @param    key     unique identifier of the variable
+       * @param    clock   new variable clock
+       * @return           new clock time for variable
+       **/
       unsigned long long set_clock (const std::string & key, 
         unsigned long long clock);
 
-      /// get the lamport clock (updates with lamport clocks lower
-      /// than our current clock get discarded)
+      /**
+       * Atomically gets the Lamport clock
+       * @return           current global clock
+       **/
       unsigned long long get_clock (void);
 
-      /// get the lamport clock for a particular variable (updates with
-      /// lamport clocks lower than our current clock get discarded)
+      /**
+       * Atomically gets the Lamport clock of a variable
+       * @param    key     unique identifier of the variable
+       * @return           current variable clock
+       **/
       unsigned long long get_clock (const std::string & key);
 
-      /// Signal the condition that it can wake up someone else 
-      /// on changed data
+      /**
+       * Signals that this thread is done with the context. Anyone
+       * waiting on the underlying condition is awoken.
+       **/
       void signal (void) const;
 
-      /// Expand a string with variable expansions
+      /**
+       * Expands a string with variable expansion. For instance, if
+       * .id == 5, and a statement of "MyVar{.id} = {.id} * 30" then
+       * then expanded statement would be "MyVar5 = 5 * 30".
+       * @param  statement  statement to expand. Useful for printing.
+       * @return            variable expanded statement
+       **/
       std::string expand_statement (const std::string & statement) const;
 
     private:
