33b01d726737d85bf36a2d5f2b9f516a0317d854
jedmondson <jedmondson@2850746a-f1c8-11dd-b507-8b4ecd5210be>
2011-12-19 18:39:08 +0000

    git-svn-id: http://madara.googlecode.com/svn/trunk@443 2850746a-f1c8-11dd-b507-8b4ecd5210be

---
+++ madara/knowledge_engine/Compiled_Expression.cpp
@@ -0,0 +1,31 @@
+#include "madara/knowledge_engine/Compiled_Expression.h"
+#include "madara/expression_tree/Expression_Tree.h"
+
+Madara::Knowledge_Engine::Compiled_Expression::Compiled_Expression ()
+{
+
+}
+
+Madara::Knowledge_Engine::Compiled_Expression::Compiled_Expression (
+  const Compiled_Expression & ce)
+: logic (ce.logic), expression (ce.expression)
+{
+
+}
+
+Madara::Knowledge_Engine::Compiled_Expression::~Compiled_Expression ()
+{
+
+}
+
+
+void
+Madara::Knowledge_Engine::Compiled_Expression::operator= (
+  const Compiled_Expression & ce)
+{
+  if (this != &ce)
+  {
+    logic = ce.logic;
+    expression = ce.expression;
+  }
+}
---
+++ madara/knowledge_engine/Compiled_Expression.h
@@ -0,0 +1,58 @@
+#ifndef _COMPILED_EXPRESSION_H_
+#define _COMPILED_EXPRESSION_H_
+
+/**
+ * @file Compiled_Expression.h
+ * @author James Edmondson <james.r.edmondson@vanderbilt.edu>
+ *
+ * This file contains the Compiled_Expression class which is used by
+ * the Madara::Knowledge_Engine::Knowledge_Base class
+ */
+
+#include <string>
+#include "madara/MADARA_export.h"
+#include "madara/expression_tree/Expression_Tree.h"
+
+namespace Madara
+{
+  namespace Knowledge_Engine
+  {
+    /**
+     * @class Compiled_Expression
+     * @brief Compiled, optimized KaRL logic
+     **/
+    class MADARA_Export Compiled_Expression
+    {
+    public:
+      //class Knowledge_Base_Impl;
+      friend class Knowledge_Base_Impl;
+      /**
+       * Constructor
+       **/
+      Compiled_Expression ();
+
+      /**
+       * Copy Constructor
+       * @param   ce      Compiled expression to copy from
+       **/
+      Compiled_Expression (const Compiled_Expression & ce);
+
+      /**
+       * Destructor
+       **/
+      ~Compiled_Expression ();
+
+      /**
+       * Assignment operator
+       * @param   ce      Compiled expression to copy from
+       **/
+      void operator= (const Compiled_Expression & ce);
+
+    private:
+      std::string logic;
+      Madara::Expression_Tree::Expression_Tree expression;
+    };
+  }
+}
+
+#endif // _COMPILED_EXPRESSION_H_
