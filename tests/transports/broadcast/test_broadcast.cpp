4b1bbca2a80ec6e81b1cf32de2fd45205f884822
jedmondson <jedmondson@2850746a-f1c8-11dd-b507-8b4ecd5210be>
2012-10-28 21:36:39 +0000

    * Added ACE broadcast and multicast tests * Added the MADARA broadcast and multicast tests
    git-svn-id: http://madara.googlecode.com/svn/trunk@510 2850746a-f1c8-11dd-b507-8b4ecd5210be

---
+++ tests/transports/broadcast/test_broadcast.cpp
@@ -0,0 +1,112 @@
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <assert.h>
+
+#include "madara/knowledge_engine/Knowledge_Base.h"
+#include "madara/utility/Log_Macros.h"
+
+std::string host ("");
+const std::string default_broadcast ("192.168.1.255:15000");
+Madara::Transport::Settings settings;
+
+void handle_arguments (int argc, char ** argv)
+{
+  for (int i = 0; i < argc; ++i)
+  {
+    std::string arg1 (argv[i]);
+
+    if (arg1 == "--h" || arg1 == "-h")
+    {
+      MADARA_DEBUG (MADARA_LOG_EMERGENCY, (LM_DEBUG, 
+        "Program arguments for %s:\n" \
+        " [-o|--host hostname]     the hostname of this process (def:localhost)\n" \
+        " [-b|--broadcast ip:port] the broadcast ip to send and listen to\n" \
+        " [-d|--domain domain]     the knowledge domain to send and listen to\n" \
+        " [-i|--id id]             the id of this agent (should be non-negative)\n" \
+        " [-l|--level level]       the logger level (0+, higher is higher detail)\n" \
+        "\n",
+        argv[0]));
+      exit (0);
+    }
+    else if (arg1 == "-b" || arg1 == "--broadcast")
+    {
+      if (i + 1 < argc)
+        settings.hosts_[0] = argv[i + 1];
+
+      ++i;
+    }
+    else if (arg1 == "-o" || arg1 == "--host")
+    {
+      if (i + 1 < argc)
+        host = argv[i + 1];
+
+      ++i;
+    }
+    else if (arg1 == "-d" || arg1 == "--domain")
+    {
+      if (i + 1 < argc)
+        settings.domains = argv[i + 1];
+
+      ++i;
+    }
+    else if (arg1 == "-i" || arg1 == "--id")
+    {
+      if (i + 1 < argc)
+      {
+        std::stringstream buffer (argv[i + 1]);
+        buffer >> settings.id;
+      }
+
+      ++i;
+    }
+    else if (arg1 == "-l" || arg1 == "--level")
+    {
+      if (i + 1 < argc)
+      {
+        std::stringstream buffer (argv[i + 1]);
+        buffer >> MADARA_debug_level;
+      }
+
+      ++i;
+    }
+  }
+}
+
+
+
+int main (int argc, char ** argv)
+{
+  settings.hosts_.resize (1);
+  settings.hosts_[0] = default_broadcast;
+  handle_arguments (argc, argv);
+
+  settings.type = Madara::Transport::BROADCAST;
+  Madara::Knowledge_Engine::Wait_Settings wait_settings;
+  wait_settings.max_wait_time = 10;
+
+  Madara::Knowledge_Engine::Knowledge_Base knowledge (host, settings);
+
+  knowledge.set (".id", (long long) settings.id);
+
+  if (settings.id == 0)
+  {
+    Madara::Knowledge_Engine::Compiled_Expression compiled = 
+      knowledge.compile ("(var2 = 1) && (var1 = 0) && var3");
+
+    knowledge.wait (compiled, wait_settings);
+  }
+  else
+  {
+    Madara::Knowledge_Engine::Compiled_Expression compiled = 
+      knowledge.compile ("!var1 && var2 => var3 = 1");
+    
+    knowledge.wait (compiled, wait_settings);
+  }
+
+  knowledge.print_knowledge ();
+
+  return 0;
+}
