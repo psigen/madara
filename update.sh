47c17dd8530bb79e262b4bf19e9c0972a200df48
snowbot <snowbot@rams.dreamhost.com>
2014-01-26 21:24:38 -0800

    Added git pull to start of update.

--- update.sh
+++ update.sh
@@ -5,15 +5,18 @@ SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update ACE
 cd ${SCRIPTPATH}/ace.git
+git pull -q origin master
 git svn rebase | grep -v "Current branch master is up to date."
 git push -q origin master --force
 
 # Update MPC
 cd ${SCRIPTPATH}/mpc.git
+git pull -q origin master
 git svn rebase | grep -v "Current branch master is up to date."
 git push -q origin master --force
 
 # Update MADARA
 cd ${SCRIPTPATH}/madara.git
+git pull -q origin master
 git svn rebase | grep -v "Current branch master is up to date."
 git push -q origin master --force
