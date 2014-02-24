5221003beaac70743c7c51c02261478d2b3836e4
jedmondson <jedmondson@2850746a-f1c8-11dd-b507-8b4ecd5210be>
2014-02-24 02:16:25 +0000

    * Updated JNI port to use appropriate style guide
    git-svn-id: http://madara.googlecode.com/svn/trunk@1249 2850746a-f1c8-11dd-b507-8b4ecd5210be

--- port/java/src/com/madara/transport/TransportSettings.java
+++ port/java/src/com/madara/transport/TransportSettings.java
@@ -10,145 +10,145 @@ import com.madara.MadaraJNI;
 
 public class TransportSettings extends MadaraJNI
 {
-  private native long jni_Settings();
-  private native long jni_Settings(long cptr);
+  private native long jni_Settings ();
+  private native long jni_Settings (long cptr);
 
-  private static native void jni_freeSettings(long cptr);
+  private static native void jni_freeSettings (long cptr);
 
-  public TransportSettings()
+  public TransportSettings ()
   {
-    setCPtr(jni_Settings());
+    setCPtr (jni_Settings ());
   }
   
-  public TransportSettings(TransportSettings transportSettings)
+  public TransportSettings (TransportSettings transportSettings)
   {
-    setCPtr(jni_Settings(transportSettings.getCPtr()));
+    setCPtr (jni_Settings (transportSettings.getCPtr ()));
   }
   
-  private native void jni_setDomains(long cptr, String domains);
-  private native String jni_getDomains(long cptr);
+  private native void jni_setDomains (long cptr, String domains);
+  private native String jni_getDomains (long cptr);
   
-  private native void jni_setQueueLength(long cptr, int queue_length);
-  private native int jni_getQueueLength(long cptr);
+  private native void jni_setQueueLength (long cptr, int queue_length);
+  private native int jni_getQueueLength (long cptr);
   
-  private native void jni_setDeadline(long cptr, int deadline);
-  private native int jni_getDeadline(long cptr);
+  private native void jni_setDeadline (long cptr, int deadline);
+  private native int jni_getDeadline (long cptr);
   
-  private native void jni_setType(long cptr, int type);
-  private native int jni_getType(long cptr);
+  private native void jni_setType (long cptr, int type);
+  private native int jni_getType (long cptr);
   
-  private native void jni_setReliability(long cptr, int reliability);
-  private native int jni_getReliability(long cptr);
+  private native void jni_setReliability (long cptr, int reliability);
+  private native int jni_getReliability (long cptr);
   
-  private native void jni_setId(long cptr, int id);
-  private native int jni_getId(long cptr);
+  private native void jni_setId (long cptr, int id);
+  private native int jni_getId (long cptr);
   
-  private native void jni_setProcesses(long cptr, int id);
-  private native int jni_getProcesses(long cptr);
+  private native void jni_setProcesses (long cptr, int id);
+  private native int jni_getProcesses (long cptr);
   
-  private native void jni_setOnDataReceivedLogic(long cptr, String onDataReceivedLogic);
-  private native String jni_getOnDataReceivedLogic(long cptr);
+  private native void jni_setOnDataReceivedLogic (long cptr, String onDataReceivedLogic);
+  private native String jni_getOnDataReceivedLogic (long cptr);
   
-  private native void jni_setHosts(long cptr, String[] hosts);
-  private native String[] jni_getHosts(long cptr);
+  private native void jni_setHosts (long cptr, String[] hosts);
+  private native String[] jni_getHosts (long cptr);
   
   
-  public void setDomains(String domains)
+  public void setDomains (String domains)
   {
-    jni_setDomains(getCPtr(), domains);
+    jni_setDomains (getCPtr (), domains);
   }
   
-  public String getDomains()
+  public String getDomains ()
   {
-    return jni_getDomains(getCPtr());
+    return jni_getDomains (getCPtr ());
   }
   
-  public void setQueueLength(int queueLength)
+  public void setQueueLength (int queueLength)
   {
-    jni_setQueueLength(getCPtr(), queueLength);
+    jni_setQueueLength (getCPtr (), queueLength);
   }
   
-  public int getQueueLength()
+  public int getQueueLength ()
   {
-    return jni_getQueueLength(getCPtr());
+    return jni_getQueueLength (getCPtr ());
   }
   
-  public void setDeadline(int deadline)
+  public void setDeadline (int deadline)
   {
-    jni_setDeadline(getCPtr(), deadline);
+    jni_setDeadline (getCPtr (), deadline);
   }
   
-  public int getDeadline()
+  public int getDeadline ()
   {
-    return jni_getDeadline(getCPtr());
+    return jni_getDeadline (getCPtr ());
   }
   
-  public void setType(TransportType transport)
+  public void setType (TransportType transport)
   {
-    jni_setType(getCPtr(), transport.value());
+    jni_setType (getCPtr (), transport.value ());
   }
   
-  public TransportType getType()
+  public TransportType getType ()
   {
-    return TransportType.getType(jni_getType(getCPtr()));
+    return TransportType.getType (jni_getType (getCPtr ()));
   }
   
-  public void setReliability(TransportReliability r)
+  public void setReliability (TransportReliability r)
   {
-    jni_setReliability(getCPtr(), r.value());
+    jni_setReliability (getCPtr (), r.value ());
   }
   
-  public TransportReliability getReliability()
+  public TransportReliability getReliability ()
   {
-    return TransportReliability.getReliability(jni_getReliability(getCPtr()));
+    return TransportReliability.getReliability (jni_getReliability (getCPtr ()));
   }
   
-  public void setId(int id)
+  public void setId (int id)
   {
-    jni_setId(getCPtr(), id);
+    jni_setId (getCPtr (), id);
   }
   
-  public int getId()
+  public int getId ()
   {
-    return jni_getId(getCPtr());
+    return jni_getId (getCPtr ());
   }
   
-  public void setProcesses(int processes)
+  public void setProcesses (int processes)
   {
-    jni_setProcesses(getCPtr(), processes);
+    jni_setProcesses (getCPtr (), processes);
   }
   
-  public int getProcesses()
+  public int getProcesses ()
   {
-    return jni_getProcesses(getCPtr());
+    return jni_getProcesses (getCPtr ());
   }
   
-  public void setOnDataReceivedLogic(String onDataReceivedLogic)
+  public void setOnDataReceivedLogic (String onDataReceivedLogic)
   {
-    jni_setOnDataReceivedLogic(getCPtr(), onDataReceivedLogic);
+    jni_setOnDataReceivedLogic (getCPtr (), onDataReceivedLogic);
   }
   
-  public String getOnDataReceivedLogic()
+  public String getOnDataReceivedLogic ()
   {
-    return jni_getOnDataReceivedLogic(getCPtr());
+    return jni_getOnDataReceivedLogic (getCPtr ());
   }
   
-  public void setHosts(String[] hosts)
+  public void setHosts (String[] hosts)
   {
-    jni_setHosts(getCPtr(), hosts);
+    jni_setHosts (getCPtr (), hosts);
   }
   
-  public String[] getHosts()
+  public String[] getHosts ()
   {
-    return jni_getHosts(getCPtr());
+    return jni_getHosts (getCPtr ());
   }
 
   /**
    * Deletes the C instantiation. To prevent memory leaks, this <b>must</b> be called
    * before an instance of WaitSettings gets garbage collected
    * */
-  public void free()
+  public void free ()
   {
-    jni_freeSettings(getCPtr());
+    jni_freeSettings (getCPtr ());
   }
 }
